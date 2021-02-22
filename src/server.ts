4d6e583a0adcf503f07320d0649bf3e658a441e2
Vitor Pontual <vitorpguedes@me.com>
2021-02-22 14:34:30 -0300

    Create first Route

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (request, response) => {
+   return response.json({message: "Hello Wolrd - NLW04"})
+});
+
+app.post('/', (request, response) => {
+   return response.json({message: 'Os dados foram salvos com sucesso!'})
+});
+
+
+app.listen(5000, () => console.log('Server is running'));
