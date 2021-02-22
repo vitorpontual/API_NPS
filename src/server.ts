62463959c56b3f30c0ad3759374af44eee64b3f1
Vitor Pontual <vitorpguedes@me.com>
2021-02-22 19:23:37 -0300

    restuaration

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,4 @@ app.post('/', (request, response) => {
    return response.json({message: 'Os dados foram salvos com sucesso!'})
 });
 
-
-app.listen(5000, () => console.log('Server is running'));
+app.listen(5000, () => console.log('Server is running!'))
