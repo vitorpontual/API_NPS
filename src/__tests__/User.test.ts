0d24fe3cf85224ec8b7c88440bdd5e505361cb55
Vitor Pontual <vitorpguedes@me.com>
2021-02-25 10:06:43 -0300

    Test settings

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -6,6 +6,9 @@ import createConnection from '../database'
 describe("User", () => {
   beforeAll(async () => {
     const connection = await createConnection();
+    await connection.query(`DELETE FROM users`)
+    await connection.query(`DELETE FROM surveys`)
+ 
 
     await connection.runMigrations();
   });
